b0e8fe4cb89cae50f6f0dfa0a71c459bdbd815ed
Intell\ASUS <yur.savinskij@gmail.com>
2022-11-22 18:39:10 +0200

    changes done

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -7,6 +7,7 @@ namespace Hello
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello twice!");
         }
     }
 }
